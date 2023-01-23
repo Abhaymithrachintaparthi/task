fadf8b3436b89f49ccd7a7747c0a6e783d8fa8c2
Abhaymithrachintaparthi <110761914+Abhaymithrachintaparthi@users.noreply.github.com>
2023-01-23 15:37:30 +0530

    Update teraform.tf

--- teraform.tf
+++ teraform.tf
@@ -1,20 +1,12 @@
-terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-    }
-  }
+provider "aws" {
+    region = "us-east-1"
+    access_key = "AKIAZJNUPWVQMA5I5CM2"
+    secret_key = "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"
 }
-resource "aws_instance" "" {
-  ami           = var.ami
-  instance_type = var.instance_type
-
-  network_interface {
-    network_interface_id = var.network_interface_id
-    device_index         = 0
-  }
-
-  credit_specification {
-    cpu_credits = "unlimited"
-  }
+resource "aws_instance" "ubuntu" {
+    ami = "ami-00874d747dde814fa"
+    instance_type = "t2.micro"
+    tags = {
+      "name" = "ec2_instance"
+    }
 }
